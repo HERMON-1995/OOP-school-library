83b641845412a40bb6f07cf84b078ea2bbf94d4c
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-18 18:49:53 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,37 +1,42 @@
 require './app'
 
-def main
-  loop do
-    app = App.new
-    puts 'Welcome to School Library'
-    puts "\n"
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
+class Main
+  def initialize
+    @app = App.new
+  end
+  puts 'Welcome to School Library'
+  def run
+    loop do
+      puts "\n"
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '7 - Exit'
 
-    input = gets.champ
+      input = gets.chomp
 
-    case input
-    when 1
-      app.list_books
-    when 2
-      app.list_people
-    when 3
-      app.create_person
-    when 4
-      app.create_book
-    when 5
-      app.reate_rental
-    when 6
-      app.list_rentals
-    when 7
-      return
+      case input.to_i
+      when 1
+        @app.list_books
+      when 2
+        @app.list_people
+      when 3
+        @app.create_person
+      when 4
+        @app.create_book
+      when 5
+        @app.create_rental
+      when 6
+        @app.list_rentals
+      when 7
+        return
+      end
     end
   end
 end
 
-main
+main = Main.new
+main.run
