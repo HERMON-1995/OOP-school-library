ad4ff88c66f89b07e7e34a72d242125476d9ddde
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-18 18:50:00 +0300

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -3,17 +3,9 @@ class Rental
     @date = date
     @book = book
     @person = person
-    @person['rentals'] << self
-    @book['rentals'] << self
+    @person.rentals << self
+    @book.rentals << self
   end
 
   attr_accessor :date, :book, :person
-
-  def to_h
-    {
-      date: @date,
-      book: @book.to_h,
-      person: @person.to_h
-    }
-  end
 end
