62ca1aec660b31a009bcb51683dea1bc636c61f5
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-18 18:49:57 +0300

    Update person.rb

--- person.rb
+++ person.rb
@@ -10,9 +10,10 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :age, :name
 
   def of_age?
@@ -23,14 +24,11 @@ class Person < Nameable
     of_age? || @parent_permission
   end
 
+  def add_rental(book, date)
+    Rental.new(date, book, self)
+  end
+
   def correct_name
     @name
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
