ac67af51c1dc76873aa352fbd1c80ed20205a03f
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-19 15:05:34 +0300

    Introduce a bug

--- book.rb
+++ book.rb
@@ -7,6 +7,8 @@ class Book
 
   def add_rental(person, date)
     Rental.new(date, self, person)
+    @rentals << rental  # Introduce a bug by appending the rental twice
+    @rentals << rental
   end
   attr_accessor :title, :author, :rentals
 end
