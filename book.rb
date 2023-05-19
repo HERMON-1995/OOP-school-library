66103ab987166e7e97a94f3b71eb168b68229314
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-19 16:08:50 +0300

    Update book.rb

--- book.rb
+++ book.rb
@@ -7,8 +7,6 @@ class Book
 
   def add_rental(person, date)
     Rental.new(date, self, person)
-    @rentals << rental  # Introduce a bug by appending the rental twice
-    @rentals << rental
   end
   attr_accessor :title, :author, :rentals
 end
