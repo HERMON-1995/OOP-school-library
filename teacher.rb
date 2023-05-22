42cb272de6218a67895bb2fbd138c2e3a8661024
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-22 15:45:15 +0300

    Update teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 # Teacher section
 
 class Teacher < Person
-  def initialize(age, specialization: nil, parent_permission: true)
+  def initialize(age, name, specialization: nil, parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
