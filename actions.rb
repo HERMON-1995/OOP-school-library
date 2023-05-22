308fc72e7717b04210063619a7c1434918f8ad53
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-05-22 15:44:54 +0300

    Create actions.rb

---
+++ actions.rb
@@ -0,0 +1,21 @@
+class Actions
+  def list_of_actions
+    puts "Choose an option from the options down below:\n"
+    option = [
+      '1 - List all books',
+      '2 - List all people',
+      '3 - Create a person',
+      '4 - Create a book',
+      '5 - Create a rental',
+      '6 - List all rentals for a given person id',
+      '7 - Exit'
+    ]
+    puts option
+  end
+
+  def get_input(prompt, type = :to_s)
+    print prompt
+    input = gets.chomp
+    type == :to_i ? input.to_i : input
+  end
+end
