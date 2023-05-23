1810f0c5f0c6ba3f4cbbfff543ad583cbaa43b74
mutai-gilbert <mutaigilbert85@gmail.com>
2023-05-23 15:44:16 +0300

    Add load_data method to data_preserving.rb file

--- data/data_preserve.rb
+++ data/data_preserve.rb
@@ -1,8 +1,16 @@
 require 'json'
 
 def save_data(data, file)
-    hashed_data = data.map(&:to_h)
-    json = JSON.generate(hashed_data)
-    File.write(file, json)
+  hashed_data = data.map(&:to_h)
+  json = JSON.generate(hashed_data)
+  File.write(file, json)
+end
+
+def load_data(file)
+  if File.exist?(file)
+    data = File.read(file)
+    JSON.parse(data)
+  else
+    []
   end
 end
